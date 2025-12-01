5b07705d54023bb19409d8cd5041662f41fc1153
Paulalex85 <tessier.paulalexandre@pm.me>
2025-12-01 19:18:24 +0100

    feat: day 1 part 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,20 +1,73 @@
-use winnow::Result;
-
 use crate::days::Day;
+use winnow::ascii::{digit1, line_ending};
+use winnow::combinator::{opt, separated, terminated};
+use winnow::token::any;
+use winnow::{Parser, Result, seq};
 
 pub struct Day01;
 
-impl Day for Day01 {
-    type Input = String;
+enum Direction {
+    Left,
+    Right,
+}
 
-    fn parser(_input: &mut &str) -> Result<Self::Input> {
-        unimplemented!("parser")
+pub struct Rotation {
+    direction: Direction,
+    steps: u32,
+}
+
+fn parse_direction(input: &mut &str) -> Result<Direction> {
+    any.verify_map(|c| match c {
+        'L' => Some(Direction::Left),
+        'R' => Some(Direction::Right),
+        _ => None,
+    })
+    .parse_next(input)
+}
+
+fn parse_rotation(input: &mut &str) -> Result<Rotation> {
+    seq! {
+        Rotation {
+            direction: parse_direction,
+            steps: digit1.parse_to(),
+        }
+    }
+    .parse_next(input)
+}
+
+impl Day for Day01 {
+    type Input = Vec<Rotation>;
+
+    fn parser(input: &mut &str) -> Result<Self::Input> {
+        Ok(terminated(
+            separated(1.., parse_rotation, line_ending),
+            opt(line_ending),
+        )
+        .parse_next(input)?)
     }
 
     type Output1 = usize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    //126.833µs
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        let mut count: u32 = 0;
+        let mut dial: i32 = 50;
+        for rotation in input.iter() {
+            match rotation.direction {
+                Direction::Left => {
+                    dial += rotation.steps as i32;
+                }
+                Direction::Right => {
+                    dial -= rotation.steps as i32;
+                }
+            }
+
+            if dial % 100 == 0 {
+                count += 1;
+            }
+        }
+
+        count as usize
     }
 
     type Output2 = usize;
