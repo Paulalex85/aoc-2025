cb5574dedc97ef09ba1cb1ce8bc7df7ace706919
Paulalex85 <tessier.paulalexandre@pm.me>
2025-12-02 00:41:20 +0100

    feat: day 1 part 2

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -72,7 +72,74 @@ impl Day for Day01 {
 
     type Output2 = usize;
 
-    fn part_2(_input: &Self::Input) -> Self::Output2 {
-        unimplemented!("part_2")
+    fn part_2(input: &Self::Input) -> Self::Output2 {
+        let mut count: u32 = 0;
+        let mut dial: u32 = 50;
+        for rotation in input.iter() {
+            let prev_dial = dial;
+            match rotation.direction {
+                Direction::Left => {
+                    let current_remaining_steps = 100 - dial;
+                    if current_remaining_steps <= rotation.steps {
+                        let remaining_steps = rotation.steps - current_remaining_steps;
+                        let rotations = remaining_steps / 100;
+                        dial = remaining_steps % 100;
+                        if dial == 0 {
+                            count += rotations;
+                        } else {
+                            count += rotations + 1;
+                        }
+                    } else {
+                        dial += rotation.steps;
+                    }
+                }
+                Direction::Right => {
+                    let current_remaining_steps = dial;
+                    if current_remaining_steps < rotation.steps {
+                        let remaining_steps = rotation.steps - current_remaining_steps;
+                        let mut rotations = remaining_steps / 100;
+                        let was_on_zero = dial == 0;
+                        dial = (100 - remaining_steps % 100) % 100;
+                        if dial != 0 {
+                            rotations += 1;
+                        }
+                        if was_on_zero {
+                            rotations -= 1;
+                        }
+                        count += rotations;
+                    } else {
+                        dial -= rotation.steps;
+                    }
+                }
+            }
+            if dial == 0 {
+                count += 1;
+            }
+            println!("{} {} {} {}", count, dial, rotation.steps, prev_dial);
+        }
+
+        count as usize
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part2() {
+        const INPUT: &str = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+
+        let parsed = Day01::parser(&mut INPUT).unwrap();
+        assert_eq!(Day01::part_2(&parsed), 6);
     }
 }
