317e55e6a889242ebe917d2a0ce2bff9f3c5b07d
Paulalex85 <tessier.paulalexandre@pm.me>
2025-12-02 14:19:46 +0100

    feat: day 2 part 2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -46,8 +46,30 @@ impl Day for Day02 {
 
     type Output2 = usize;
 
-    fn part_2(_input: &Self::Input) -> Self::Output2 {
-        unimplemented!("part_2")
+    fn part_2(input: &Self::Input) -> Self::Output2 {
+        input
+            .into_iter()
+            .map(|ids| {
+                let values: Vec<u64> = (ids.value.0..=ids.value.1).collect();
+                values
+                    .iter()
+                    .filter(|x| {
+                        let x = x.to_string();
+
+                        for i in 1..=x.len() / 2 {
+                            if x.len() % i != 0 {
+                                continue;
+                            }
+                            if (1..x.len() / i).all(|j| &x[0..i] == &x[j * i..=((j + 1) * i) - 1]) {
+                                return true;
+                            }
+                        }
+                        return false;
+                    })
+                    .map(|x| *x as usize)
+                    .sum::<usize>()
+            })
+            .sum::<usize>()
     }
 }
 
@@ -61,5 +83,13 @@ mod tests {
 
         let parsed = Day02::parser(&mut INPUT).unwrap();
         assert_eq!(Day02::part_1(&parsed), 1227775554);
+    }
+
+    #[test]
+    fn test_part2() {
+        const INPUT: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+        let parsed = Day02::parser(&mut INPUT).unwrap();
+        assert_eq!(Day02::part_2(&parsed), 4174379265);
     }
 }
