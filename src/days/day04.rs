e5ced998311767bee5127d11d86f846af0063b55
Paulalex85 <tessier.paulalexandre@pm.me>
2025-12-04 10:47:05 +0100

    feat: day 4 part 1

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,20 +1,74 @@
-use winnow::Result;
-
 use crate::days::Day;
+use winnow::ascii::line_ending;
+use winnow::combinator::{opt, separated, terminated};
+use winnow::token::take_while;
+use winnow::{Parser, Result};
 
 pub struct Day04;
 
-impl Day for Day04 {
-    type Input = String;
+fn parse_line(input: &mut &str) -> Result<Vec<char>> {
+    take_while(1.., |c: char| c == '@' || c == '.')
+        .map(|s: &str| s.chars().collect())
+        .parse_next(input)
+}
 
-    fn parser(_input: &mut &str) -> Result<Self::Input> {
-        unimplemented!("parser")
+impl Day for Day04 {
+    type Input = Vec<Vec<char>>;
+
+    fn parser(input: &mut &str) -> Result<Self::Input> {
+        Ok(
+            terminated(separated(1.., parse_line, line_ending), opt(line_ending))
+                .parse_next(input)?,
+        )
     }
 
     type Output1 = usize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        let mut result = 0;
+        let width = input.get(0).unwrap().len();
+        let height = input.len();
+        for i in 0..height {
+            for j in 0..width {
+                if input.get(i).unwrap().get(j).unwrap() != &'@' {
+                    continue;
+                }
+                let mut count = 0;
+                if j > 0 && i > 0 && input.get(i - 1).unwrap().get(j - 1).unwrap() == &'@' {
+                    count += 1;
+                }
+                if i > 0 && input.get(i - 1).unwrap().get(j).unwrap() == &'@' {
+                    count += 1;
+                }
+                if j < width - 1 && i > 0 && input.get(i - 1).unwrap().get(j + 1).unwrap() == &'@' {
+                    count += 1;
+                }
+                if j > 0 && input.get(i).unwrap().get(j - 1).unwrap() == &'@' {
+                    count += 1;
+                }
+                if j < width - 1 && input.get(i).unwrap().get(j + 1).unwrap() == &'@' {
+                    count += 1;
+                }
+                if j > 0 && i < height - 1 && input.get(i + 1).unwrap().get(j - 1).unwrap() == &'@'
+                {
+                    count += 1;
+                }
+                if i < height - 1 && input.get(i + 1).unwrap().get(j).unwrap() == &'@' {
+                    count += 1;
+                }
+                if j < width - 1
+                    && i < height - 1
+                    && input.get(i + 1).unwrap().get(j + 1).unwrap() == &'@'
+                {
+                    count += 1;
+                }
+                if count < 4 {
+                    result += 1;
+                }
+            }
+        }
+
+        result
     }
 
     type Output2 = usize;
