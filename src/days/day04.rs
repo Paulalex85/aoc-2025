51be3301fafeeca74368cb10f1d387f4facb8b81
Paulalex85 <tessier.paulalexandre@pm.me>
2025-12-04 11:06:07 +0100

    feat: day 4 part 2

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -25,55 +25,72 @@ impl Day for Day04 {
     type Output1 = usize;
 
     fn part_1(input: &Self::Input) -> Self::Output1 {
-        let mut result = 0;
-        let width = input.get(0).unwrap().len();
-        let height = input.len();
-        for i in 0..height {
-            for j in 0..width {
-                if input.get(i).unwrap().get(j).unwrap() != &'@' {
-                    continue;
-                }
-                let mut count = 0;
-                if j > 0 && i > 0 && input.get(i - 1).unwrap().get(j - 1).unwrap() == &'@' {
-                    count += 1;
-                }
-                if i > 0 && input.get(i - 1).unwrap().get(j).unwrap() == &'@' {
-                    count += 1;
-                }
-                if j < width - 1 && i > 0 && input.get(i - 1).unwrap().get(j + 1).unwrap() == &'@' {
-                    count += 1;
-                }
-                if j > 0 && input.get(i).unwrap().get(j - 1).unwrap() == &'@' {
-                    count += 1;
-                }
-                if j < width - 1 && input.get(i).unwrap().get(j + 1).unwrap() == &'@' {
-                    count += 1;
-                }
-                if j > 0 && i < height - 1 && input.get(i + 1).unwrap().get(j - 1).unwrap() == &'@'
-                {
-                    count += 1;
-                }
-                if i < height - 1 && input.get(i + 1).unwrap().get(j).unwrap() == &'@' {
-                    count += 1;
-                }
-                if j < width - 1
-                    && i < height - 1
-                    && input.get(i + 1).unwrap().get(j + 1).unwrap() == &'@'
-                {
-                    count += 1;
-                }
-                if count < 4 {
-                    result += 1;
-                }
-            }
-        }
-
-        result
+        let mut input_copy = input.clone();
+        count_and_remove_rolls(&mut input_copy)
     }
 
     type Output2 = usize;
 
-    fn part_2(_input: &Self::Input) -> Self::Output2 {
-        unimplemented!("part_2")
+    fn part_2(input: &Self::Input) -> Self::Output2 {
+        let mut input_copy = input.clone();
+        let mut result = 0;
+        let mut last_result = 1;
+        while last_result > 0 {
+            last_result = count_and_remove_rolls(&mut input_copy);
+            result += last_result;
+        }
+        result
     }
+}
+
+fn count_and_remove_rolls(input: &mut Vec<Vec<char>>) -> usize {
+    let mut result = 0;
+    let mut rolls_to_remove: Vec<(usize, usize)> = vec![];
+    let width = input.get(0).unwrap().len();
+    let height = input.len();
+    for i in 0..height {
+        for j in 0..width {
+            if input.get(i).unwrap().get(j).unwrap() != &'@' {
+                continue;
+            }
+            let mut count = 0;
+            if j > 0 && i > 0 && input.get(i - 1).unwrap().get(j - 1).unwrap() == &'@' {
+                count += 1;
+            }
+            if i > 0 && input.get(i - 1).unwrap().get(j).unwrap() == &'@' {
+                count += 1;
+            }
+            if j < width - 1 && i > 0 && input.get(i - 1).unwrap().get(j + 1).unwrap() == &'@' {
+                count += 1;
+            }
+            if j > 0 && input.get(i).unwrap().get(j - 1).unwrap() == &'@' {
+                count += 1;
+            }
+            if j < width - 1 && input.get(i).unwrap().get(j + 1).unwrap() == &'@' {
+                count += 1;
+            }
+            if j > 0 && i < height - 1 && input.get(i + 1).unwrap().get(j - 1).unwrap() == &'@' {
+                count += 1;
+            }
+            if i < height - 1 && input.get(i + 1).unwrap().get(j).unwrap() == &'@' {
+                count += 1;
+            }
+            if j < width - 1
+                && i < height - 1
+                && input.get(i + 1).unwrap().get(j + 1).unwrap() == &'@'
+            {
+                count += 1;
+            }
+            if count < 4 {
+                result += 1;
+                rolls_to_remove.push((i, j));
+            }
+        }
+    }
+
+    for roll in rolls_to_remove {
+        input[roll.0][roll.1] = '.';
+    }
+
+    result
 }
