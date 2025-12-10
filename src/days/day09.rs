ef3620ae338c8c068eeee82596f589466885a202
Paulalex85 <tessier.paulalexandre@pm.me>
2025-12-10 18:42:57 +0100

    feat: day 9 part 1

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,20 +1,38 @@
-use winnow::Result;
+use winnow::ascii::{digit1, line_ending};
+use winnow::combinator::{opt, separated, separated_pair, terminated};
+use winnow::{Parser, Result};
 
 use crate::days::Day;
 
 pub struct Day09;
 
-impl Day for Day09 {
-    type Input = String;
+fn parse_line(input: &mut &str) -> Result<(u64, u64)> {
+    separated_pair(digit1.parse_to(), ',', digit1.parse_to()).parse_next(input)
+}
 
-    fn parser(_input: &mut &str) -> Result<Self::Input> {
-        unimplemented!("parser")
+impl Day for Day09 {
+    type Input = Vec<(u64, u64)>;
+
+    fn parser(input: &mut &str) -> Result<Self::Input> {
+        terminated(separated(1.., parse_line, line_ending), opt(line_ending)).parse_next(input)
     }
 
     type Output1 = usize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        input
+            .iter()
+            .enumerate()
+            .map(|(i, &(x, y))| {
+                input
+                    .iter()
+                    .skip(i)
+                    .map(|&(x2, y2)| (x.abs_diff(x2) + 1) * (y.abs_diff(y2) + 1))
+                    .max()
+                    .unwrap()
+            })
+            .max()
+            .unwrap() as usize
     }
 
     type Output2 = usize;
