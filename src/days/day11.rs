f44e25f603862556bab59ce90143635398c23e98
Paulalex85 <tessier.paulalexandre@pm.me>
2025-12-14 13:41:46 +0100

    feat: day 11 part 1

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,20 +1,55 @@
-use winnow::Result;
-
 use crate::days::Day;
+use std::collections::HashMap;
+use winnow::ascii::{alphanumeric1, line_ending, space1};
+use winnow::combinator::{opt, preceded, separated, terminated};
+use winnow::token::literal;
+use winnow::{Parser, Result};
 
 pub struct Day11;
 
-impl Day for Day11 {
-    type Input = String;
+fn parse_line(input: &mut &str) -> Result<(String, Vec<String>)> {
+    (
+        alphanumeric1.map(str::to_string),
+        preceded(
+            literal(':'),
+            preceded(
+                space1,
+                separated(1.., alphanumeric1.map(str::to_string), space1),
+            ),
+        ),
+    )
+        .parse_next(input)
+}
 
-    fn parser(_input: &mut &str) -> Result<Self::Input> {
-        unimplemented!("parser")
+impl Day for Day11 {
+    type Input = HashMap<String, Vec<String>>;
+
+    fn parser(input: &mut &str) -> Result<Self::Input> {
+        terminated(separated(1.., parse_line, line_ending), opt(line_ending))
+            .map(|lines: Vec<_>| lines.into_iter().collect())
+            .parse_next(input)
     }
 
     type Output1 = usize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        let mut found = 0;
+        let mut path_pile: Vec<Vec<String>> = vec![vec!["you".to_string()]];
+
+        while let Some(path) = path_pile.pop() {
+            let last = path.last().unwrap();
+            let next_paths = input.get(last).unwrap();
+            for next_path in next_paths {
+                if next_path == "out" {
+                    found += 1;
+                } else if !path.contains(&next_path) {
+                    let mut temp = path.clone();
+                    temp.push(next_path.clone());
+                    path_pile.push(temp);
+                }
+            }
+        }
+        found
     }
 
     type Output2 = usize;
